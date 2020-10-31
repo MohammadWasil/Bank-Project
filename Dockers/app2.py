1b5a78a29aa550e2f9e168a064eeff1c47546108
MohammadWasil <wasilmohd1@gmail.com>
2020-10-31 18:57:53 +0100

    update on try and except

--- Dockers/app2.py
+++ Dockers/app2.py
@@ -22,35 +22,42 @@ def register_here_page():
     
 @app.route('/register_username', methods = ["POST"])
 def register_users():
-
-    first_name_reg = request.form["first_name_reg"]
-    last_name_reg = request.form["last_name_reg"]
-    username_reg = request.form["username_reg"]
-    password_reg = request.form["password_reg"]
+    try:
+        first_name_reg = request.form["first_name_reg"]
+        last_name_reg = request.form["last_name_reg"]
+        username_reg = request.form["username_reg"]
+        password_reg = request.form["password_reg"]
+        
+        document = { "first name" : first_name_reg, "last name" : last_name_reg, 
+                    "username" : username_reg, "password" : password_reg}
+        
+        collection_username.insert_one(document)
+        
+        return render_template('register_page.html', registered = True)
     
-    document = { "first name" : first_name_reg, "last name" : last_name_reg, 
-                "username" : username_reg, "password" : password_reg}
-    
-    collection_username.insert_one(document)
-    
-    return render_template('register_page.html', registered = True)
+    except Exception as register_username_exception:
+        return "Error : " + str(register_username_exception)
 
 @app.route('/login_sucess', methods = ["POST"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    
-    # Check if the user login and password are correct or not.
-    query_username = { "username" : username}
-    your_username = collection_username.find(query_username)
-    
-    for record in your_username:    
-        if record["password"] == password:
-            print("password is correct")
-            return render_template('logged_in.html')
-        else:
-            print("The username and password that you entered is wrong!")
-            return render_template("index.html", wrong_info = True)
+    try:
+
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        # Check if the user login and password are correct or not.
+        query_username = { "username" : username}
+        your_username = collection_username.find(query_username)
+        
+        for record in your_username:    
+            if record["password"] == password:
+                print("password is correct")
+                return render_template('logged_in.html')
+            else:
+                print("The username and password that you entered is wrong!")
+                return render_template("index.html", wrong_info = True)
+    except Exception as error:
+        return "Error : " + str(error)
     
 
 # transaction_hisory_page
@@ -60,23 +67,27 @@ def transaction_history():
         row = collection_customer_transaction.find() 
         return render_template('transaction_history.html', documents=row)
     except Exception as e:
-        return "Error 404" + str(e)
+        return "Error : " + str(e)
     
 @app.route('/update_transaction', methods=["POST"])
 def update_transaction():
-    SNo = request.form["SNo"]
-    date = request.form["date"]
-    place = request.form["place"]
-    item = request.form["item"]
-    amount = request.form["amount"]
-    total = request.form["total"]
-    
-    document = {"SNo" : SNo, "Date" : date, "Place" : place, "Item" : item, "Amount" : amount, "Total" : total}
+    try:
 
-    collection_customer_transaction.insert_one(document)
+        SNo = request.form["SNo"]
+        date = request.form["date"]
+        place = request.form["place"]
+        item = request.form["item"]
+        amount = request.form["amount"]
+        total = request.form["total"]
+        
+        document = {"SNo" : SNo, "Date" : date, "Place" : place, "Item" : item, "Amount" : amount, "Total" : total}
+
+        collection_customer_transaction.insert_one(document)
+        
+        return render_template('logged_in.html')
     
-    return render_template('logged_in.html')
-    
+    except Exception as update_error:
+        return "Error : " + str(update_error)
     
 # Using html syntax
 #@app.route("/form", methods=["GET"])
