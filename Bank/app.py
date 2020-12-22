b1f72cb9dfd91268ab775800b24c5990879565ae
MohammadWasil <wasilmohd1@gmail.com>
2020-12-22 20:44:18 +0100

    update

--- Bank/app.py
+++ Bank/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import pandas as pd
 import pickle
 from pymongo import MongoClient 
+import json
 
 app = Flask(__name__)
 
@@ -22,28 +23,35 @@ def register_here_page():
 @app.route('/register_username', methods = ["POST"])
 def register_users():
     try:
-        first_name_reg = request.form["first_name_reg"]
-        last_name_reg = request.form["last_name_reg"]
-        username_reg = request.form["username_reg"]
-        password_reg = request.form["password_reg"]
+        #user_credentials = jsonify(request.form)
+        user_signup_credentials = json.loads(json.dumps(request.form))
+        first_name_reg = user_signup_credentials["first_name_reg"]
+        last_name_reg = user_signup_credentials["last_name_reg"]
+        username_reg = user_signup_credentials["username_reg"]
+        password_reg = user_signup_credentials["password_reg"]
         
         document = { "first name" : first_name_reg, "last name" : last_name_reg, 
                     "username" : username_reg, "password" : password_reg}
         
         collection_username.insert_one(document)
-        
+
         return render_template('register_page.html', registered = True)
     
     except Exception as register_username_exception:
-        return "Error : " + str(register_username_exception)
+        return jsonify({"Error:" : str(register_username_exception)} )
 
 @app.route('/login_sucess', methods = ["POST"])
 def login():
+    
     try:
-
-        username = request.form["username"]
-        password = request.form["password"]
+        user_login_credentials = json.loads(json.dumps(request.form))
+        username = user_login_credentials["username"]
+        password = user_login_credentials["password"]
         
+        #username = user_login_credentials.get("username")
+        #password = user_login_credentials.get("password")
+        #print(username)
+
         # Check if the user login and password are correct or not.
         query_username = { "username" : username}
         your_username = collection_username.find(query_username)
@@ -56,7 +64,7 @@ def login():
                 print("The username and password that you entered is wrong!")
                 return render_template("index.html", wrong_info = True)
     except Exception as error:
-        return "Error : " + str(error)
+        return jsonify({"Error " : str(error)})
     
 
 # transaction_hisory_page
@@ -66,18 +74,19 @@ def transaction_history():
         row = collection_customer_transaction.find() 
         return render_template('transaction_history.html', documents=row)
     except Exception as e:
-        return "Error : " + str(e)
+        return jsonify({"Error " : str(e)})
     
 @app.route('/update_transaction', methods=["POST"])
 def update_transaction():
     try:
-
-        SNo = request.form["SNo"]
-        date = request.form["date"]
-        place = request.form["place"]
-        item = request.form["item"]
-        amount = request.form["amount"]
-        total = request.form["total"]
+        update_history = json.loads(json.dumps(request.form))
+        
+        SNo     = update_history["SNo"]
+        date    = update_history["date"]
+        place   = update_history["place"]
+        item    = update_history["item"]
+        amount  = update_history["amount"]
+        total   = update_history["total"]
         
         document = {"SNo" : SNo, "Date" : date, "Place" : place, "Item" : item, "Amount" : amount, "Total" : total}
 
@@ -86,7 +95,7 @@ def update_transaction():
         return render_template('logged_in.html')
     
     except Exception as update_error:
-        return "Error : " + str(update_error)
+        return jsonify({"Error " : str(update_error)})
     
 # Using html syntax
 #@app.route("/form", methods=["GET"])
@@ -94,4 +103,4 @@ def update_transaction():
 #    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
